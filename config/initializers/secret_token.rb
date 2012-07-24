ff7fd338528e956638b11f30e209fed911bc70c7
Edson Ninomiya <kenjinino07@gmail.com>
2012-07-24 20:22:50 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeManagementMatrix::Application.config.secret_token = 'c6384c8fbee2d5cf90a8c6f99eb9379ff962407c97b16f99d5a8953cf0495fbb2a04e870d08983adfe33e9200b423923abc02fcda5493aaebbe2d08f56fee27a'
